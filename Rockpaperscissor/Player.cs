23535e173d74cdf9d83629d04b1617f5e58bd3b6
heke99 <heke99@live.se>
2023-09-26 15:30:30 +0200

    initial commit

---
+++ Rockpaperscissor/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace Rockpaperscissor;
+
+class Random = new Random(choice);
+
+ list = new List<choice>
+
+public class player 
+{
+
+     player player1 = new player("x",choice.random)
+     player player2 = new player
+
+}
+
---
+++ Rockpaperscissor/result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Rockpaperscissor
+{
+	public enum result
+	{
+		win,
+		loss,
+		draw,
+	}
+
+	public enum choice
+
+	{
+		Rock,
+		paper,
+		scissor,
+	}
+}
+
+
+
+
